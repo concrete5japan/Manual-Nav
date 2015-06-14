8037d039f9af5a81bdb5e68f34e7aaecf9fef14d
acliss19xx <acliss19xx@gmail.com>
2015-06-14 21:07:16 +0900

    add class "isFirst"

--- manual_nav/blocks/manual_nav/view.php
+++ manual_nav/blocks/manual_nav/view.php
@@ -2,10 +2,12 @@
 $c = Page::getCurrentPage();
 ?>
 
-    <?php if (count($rows) > 0) { ?>
+    <?php if (count($rows) > 0) {
+	$rows[0]['class'] = "nav-first";
+    ?>
     <ul class="nav">
     <?php foreach ($rows as $row) { ?>
-	    <li>
+	    <li class="<?php echo $row['class']?>">
 		<a href="<?php echo $row['linkURL'] ?>"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']); ?></a>
 	    </li>
     <?php } ?>
