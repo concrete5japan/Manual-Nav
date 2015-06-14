3dd6e6182d6f58f9204c4c0406b1e8076b04ba87
acliss19xx <acliss19xx@gmail.com>
2015-06-14 19:10:02 +0900

    Remove unnecessary html tags #1

--- manual_nav/blocks/manual_nav/view.php
+++ manual_nav/blocks/manual_nav/view.php
@@ -2,24 +2,16 @@
 $c = Page::getCurrentPage();
 ?>
 
-<div class="ccm-manual-nav-container ccm-block-manual-nav" >
-    <div class="ccm-manual-nav">
-        <div class="ccm-manual-nav-inner">
-
-        <?php if(count($rows) > 0) { ?>
-        <ul id="ccm-manual-nav-<?php echo $bID ?>">
-            <?php foreach($rows as $row) { ?>
-                <li>
-                    <a href="<?php echo $row['linkURL'] ?>" class="mega-link-overlay"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']);  ?></a>
-                </li>
-            <?php } ?>
-        </ul>
-        <?php } else { ?>
-        <div class="ccm-manual-nav-placeholder">
-            <p><?php echo t('No nav Entered.'); ?></p>
-        </div>
-        <?php } ?>
-        </div>
-
+    <?php if (count($rows) > 0) { ?>
+    <ul class="nav">
+    <?php foreach ($rows as $row) { ?>
+	    <li>
+		<a href="<?php echo $row['linkURL'] ?>"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']); ?></a>
+	    </li>
+    <?php } ?>
+    </ul>
+<?php } else { ?>
+    <div class="ccm-manual-nav-placeholder">
+        <p><?php echo t('No nav Entered.'); ?></p>
     </div>
-</div>
+<?php } ?>
