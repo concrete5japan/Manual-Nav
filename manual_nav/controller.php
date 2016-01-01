eef0641909df8ef6c5267b8963a53c203791e8be
acliss19xx <acliss19xx@gmail.com>
2016-01-02 08:32:42 +0900

    ｖersion up 1.0.2

--- manual_nav/controller.php
+++ manual_nav/controller.php
@@ -13,7 +13,7 @@ class Controller extends Package
 
 	protected $pkgHandle = 'manual_nav';
 	protected $appVersionRequired = '5.7.1';
-	protected $pkgVersion = '1.0.1';
+	protected $pkgVersion = '1.0.2';
 	protected static $blockTypes = array(
         array(
 			'handle' => 'manual_nav', 'set' => 'navigation',
